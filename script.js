29f8a977c3aae1601a07bd9683bb5a391908890f
Syed Talha <syedt.in00@gmail.com>
2023-10-16 21:34:43 +0500

    updated Calculator stracture

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-let result = "0";
+// let result = "0";
 
-function appendToResult(value) {
-    if (result === "0" && value !== ".") {
-        result = "";
-    }
-    result += value;
-    updateDisplay();
-}
+// function appendToResult(value) {
+//     if (result === "0" && value !== ".") {
+//         result = "";
+//     }
+//     result += value;
+//     updateDisplay();
+// }
 
-function clearResult() {
-    result = "0";
-    updateDisplay();
-}
+// function clearResult() {
+//     result = "0";
+//     updateDisplay();
+// }
 
-function calculateResult() {
-    try {
-        result = eval(result).toString();
-    } catch (error) {
-        result = "Error";
-    }
-    updateDisplay();
-}
+// function calculateResult() {
+//     try {
+//         result = eval(result).toString();
+//     } catch (error) {
+//         result = "Error";
+//     }
+//     updateDisplay();
+// }
 
-function updateDisplay() {
-    document.getElementById("result").innerText = result;
-}
+// function updateDisplay() {
+//     document.getElementById("result").innerText = result;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
