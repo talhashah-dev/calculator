98b107007d4b94983ecd554a28e738b082839c16
Syed Talha <syedt.in00@gmail.com>
2023-10-21 01:44:58 +0500

    modified all three files

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
+let operator = ["%", "*", "+", "-", "="];
+let output = "";
+
+let calculate = (btnValue) => {
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+        output = "";
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && operator.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
